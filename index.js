da26843d6de1a06f5b77425a9fecba452f2c9737
Charles Anderson <charlesandreanderson22@gmail.com>
2017-10-03 15:59:43 -0700

    finshed adding routes

--- index.js
+++ index.js
@@ -3,4 +3,35 @@ var lodash = require('lodash');
 var chalk = require('chalk');
 var express = require('express');
 var bodyParser = require('body-parser');
+const PORT = 3000;
+
+const app = express();
+
+var movies = ["T2", "The land before time", "Scary Movie"];
+
+app.get('/', (req, res) => {
+    // 
+});
+
+app.get('/movies/:id', (req, res) => {
+    // grab single moview
+});
+
+app.post('/movies/:name', (req, res) => {
+    // add movie
+});
+
+app.delete('/movies/:name', (req, res) => {
+    // delete movie
+});
+
+app.put('/movies/:id/edit', (req, res) => {
+    // edit movie
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ' + PORT)
+})
 
