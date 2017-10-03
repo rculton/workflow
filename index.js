b0de61bba5cf3205aefe5035bb54b13185ba50ad
Ryan Culton <rculton@gmail.com>
2017-10-03 16:06:45 -0700

    Added functions to routes

--- index.js
+++ index.js
@@ -10,23 +10,26 @@ const app = express();
 var movies = ["T2", "The land before time", "Scary Movie"];
 
 app.get('/', (req, res) => {
-    // 
+    res.send(movies)
 });
 
 app.get('/movies/:id', (req, res) => {
-    // grab single moview
+    res.send(movies[req.params['id']])
 });
 
 app.post('/movies/:name', (req, res) => {
-    // add movie
+    movies.push(req.params['name'])
+    res.send(movies)
 });
 
 app.delete('/movies/:name', (req, res) => {
-    // delete movie
+    movies.splice(movies.indexOf(req.params['name']),1)
+    res.send(movies)
 });
 
 app.put('/movies/:id/edit', (req, res) => {
-    // edit movie
+    movies[req.params['id']] = req.params['edit']
+    res.send(movies)
 })
 
 
